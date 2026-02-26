6335fd30d09fa21bd4c213cd4255392bb57b1dcd
zac <abderrazaklahcene90@gmail.com>
2026-02-26 20:34:36 +0100

    second commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,66 @@
 #include <iostream>
-int main () {
-    std::cout << "hey there";
-    return 0;
-}
+using namespace std;
 
-// g++ main.cpp -o program
-// ./program
 
+// Create variables to store:
+
+// Your age
+
+// Your height (in meters)
+
+// The first letter of your name
+
+// A boolean that tells if you are a student
+
+// Then print them.
+
+// int main () {
+//     int age = 33;
+//     float height = 1.80;
+//     char letter = 'a';
+//     bool student = true;
+    
+//     cout << "my age is " << age << "\n";
+//     cout << "my height is " << height << "\n";
+//     cout << "my first letter of my name " << letter << "\n";
+//     cout << student << "\n";
+//     return 0;
+// }
+
+// ///////////////////////////////////////////////
+
+// Declare:
+
+// Two integers a and b
+
+// Store 10 and 20
+
+// Print their sum
+
+// ///////////////////////////////////////////////
+
+// Create:
+
+// A double price = 99.99
+
+// An int quantity = 3
+
+// Create a variable total and calculate total price.
+// Print the result.
+
+// ///////////////////////////////////////////////
+
+// Declare 3 integers:
+
+// length
+
+// width
+
+// area
+
+// Calculate area of a rectangle.
+
+// ///////////////////////////////////////////////
+
+// Create a constant variable PI = 3.14159
+// Use it to calculate area of a circle (radius = 5).
